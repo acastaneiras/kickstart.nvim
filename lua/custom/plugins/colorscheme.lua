ab9dfab1a85c33cc618aab5375edf039d03ffcaf
acastaneiras <33935297+acastaneiras@users.noreply.github.com>
2025-07-27 13:02:06 +0200

    scheme changes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -15,6 +15,10 @@ return {
         },
         overrides = function(colors)
           local theme = colors.theme
+          local makeDiagnosticColor = function(color)
+            local c = require 'kanagawa.lib.color'
+            return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+          end
           return {
             -- Window
             Normal = { bg = 'none' },
@@ -40,6 +44,17 @@ return {
 
             LazyNormal = { bg = 'none' },
             MasonNormal = { bg = 'none' },
+
+            NoiceFormatProgressDone = { fg = colors.theme.syn.string, bg = 'none' },
+            NoiceFormatProgressTodo = { fg = colors.theme.syn.comment, bg = 'none' },
+            NoiceLspProgressSpinner = { fg = colors.theme.syn.constant, bg = 'none' },
+            NoiceLspProgressClient = { fg = colors.theme.syn.identifier, bg = 'none' },
+            NoiceLspProgressTitle = { fg = colors.theme.syn.statement, bg = 'none' },
+
+            DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+            DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+            DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+            DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
           }
         end,
       }
