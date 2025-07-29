edca10266045d84b0d7a3461aacbe2db3e92f06d
acastaneiras <33935297+acastaneiras@users.noreply.github.com>
2025-07-29 23:38:02 +0200

    change desc

--- lua/custom/plugins/gitblame.lua
+++ lua/custom/plugins/gitblame.lua
@@ -7,7 +7,7 @@ return {
       vim.g.gitblame_use_blame_commit_file_urls = true
       vim.cmd [[autocmd BufEnter * GitBlameDisable]]
       vim.keymap.set('n', '<leader>hb', ':GitBlameToggle<CR>', { desc = 'git [b]lame line' })
-      vim.keymap.set('n', '<leader>go', ':GitBlameOpenCommitURL<CR>', { desc = 'Abre commit del blame en navegador' })
+      vim.keymap.set('n', '<leader>go', ':GitBlameOpenCommitURL<CR>', { desc = 'Opens commit in browser' })
     end,
   },
 }
