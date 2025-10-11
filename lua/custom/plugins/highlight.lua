6a845a7e317854c02f2357bf84a536f7d801cb42
acastaneiras <33935297+acastaneiras@users.noreply.github.com>
2025-10-11 14:38:18 +0200

    remove tailwind-tools

--- lua/custom/plugins/highlight.lua
+++ lua/custom/plugins/highlight.lua
@@ -1,20 +1,9 @@
 return {
   {
-    'luckasRanarison/tailwind-tools.nvim',
-    name = 'tailwind-tools',
-    build = ':UpdateRemotePlugins',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-telescope/telescope.nvim', -- optional
-      'neovim/nvim-lspconfig', -- optional
-    },
-    opts = {}, -- your configuration
-  },
-  {
     'brenoprata10/nvim-highlight-colors',
     config = function()
       require('nvim-highlight-colors').setup {
-        enable_tailwind = false, --- Disabling Tailwind integration since I use tailwind tools for that
+        enable_tailwind = true,
       }
     end,
   },
