6ab1e157f75b96a0fbce6a0ea07ace7aeaf30364
acastaneiras <33935297+acastaneiras@users.noreply.github.com>
2025-11-14 19:58:17 +0100

    add prettier config

--- init.lua
+++ init.lua
@@ -834,14 +834,28 @@ require('lazy').setup({
           }
         end
       end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
+      formatters_by_ft = (function()
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
+        local prettier_fts = {
+          'javascript',
+          'javascriptreact',
+          'typescript',
+          'typescriptreact',
+          'json',
+          'html',
+          'css',
+          'markdown',
+        }
+        local config = { lua = { 'stylua' } }
+        for _, filetype in ipairs(prettier_fts) do
+          config[filetype] = { 'prettierd' }
+        end
+        return config
+      end)(),
     },
   },
 
